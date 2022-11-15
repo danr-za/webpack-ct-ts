a1607a379ce1fd0733c19d5c0779a0c765de36af
Dan Roujinsky <d.roujinsky@island.io>
2022-11-15 16:32:20 +0200

    add webpack

---
+++ apps/my-app/webpack.config.ts
@@ -0,0 +1,11 @@
+import { Configuration } from 'webpack'
+
+export default function (configuration: Configuration) {
+  if(configuration.mode === 'production') {
+    console.log('Prod!')
+  } else {
+    console.log('not prod!')
+  }
+
+  return configuration
+}
